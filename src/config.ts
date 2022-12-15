1af1804487a200910c60cae01e9fded210dde745
Hoang Do <nkhdo94@gmail.com>
2022-12-15 13:03:05 +0700

    chore: init

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Hoangf';
+export const SITE_DESCRIPTION = 'Hoang Do\'s personal space';
